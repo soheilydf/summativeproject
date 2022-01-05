01153e27d807816604e2cf3c2439e1a711332bee
soheilydf <97129446+soheilydf@users.noreply.github.com>
2022-01-06 00:18:13 +0330

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
 print("i have changed the code again *2   ") 
+def q1_function(intlist,strlist):                
+        if (len(intlist) == len(strlist)):
+            newlist = []
+            count = 0
+            while(count < len(intlist)):           
+                item1 = str(intlist[count])       
+                item2 = strlist[count] 
+                item3 = item1 + item2      
+                newlist.append(item3)    
+                count += 1
+            print("complete!")
+            print("The new list is: ",newlist)
+            return newlist
+        elif(len(intlist) != len(strlist)):
+            print("sorry this function will not work")
